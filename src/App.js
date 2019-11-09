bb502ef456eb9c111b9a79a1f0585873d4074337
Victor Tran <victor.n.tran@outlook.com>
2019-11-08 20:01:17 -0800

    build exo skeleton of things to do

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  state = {
+    todos: []
+  };
+
+  createTodo = (props) => {};
+
+  addTodo = (todo) => {};
+
+  updateTodo = (id, action) => {}
+
+  clearTodo = () => {}
+
+  displayTodo = () => {};
+
+  handleSubmit = e => {};
+
+  handleChange = e => {};
+
   render() {
     return (
       <div>
